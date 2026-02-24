dd5f8e286cdaa5b3ed936ed58abb543071037779
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2026-02-24 15:10:38 -0300

    Criação do projeto habibs

---
+++ habibs/src/main/java/com/monitoria/habibs/HabibsApplication.java
@@ -0,0 +1,13 @@
+package com.monitoria.habibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabibsApplication.class, args);
+	}
+
+}
---
+++ habibs/src/test/java/com/monitoria/habibs/HabibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoria.habibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
