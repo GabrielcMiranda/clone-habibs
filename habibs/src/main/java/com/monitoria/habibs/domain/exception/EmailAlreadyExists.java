143eb5561e988335850a6d8d9a8ef0d234eeaa29
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2026-03-18 19:24:38 -0300

    feat: adicionar exceção EmailAlreadyExists

---
+++ habibs/src/main/java/com/monitoria/habibs/domain/exception/EmailAlreadyExists.java
@@ -0,0 +1,8 @@
+package com.monitoria.habibs.domain.exception;
+
+public class EmailAlreadyExists extends RuntimeException {
+    public EmailAlreadyExists(String message) {
+        super(message);
+    }
+    
+}
