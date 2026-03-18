fe5b89f7cd0bfd806aeaac0187c84672a467f444
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2026-03-18 19:24:24 -0300

    feat: adicionar GetUserUseCase

---
+++ habibs/src/main/java/com/monitoria/habibs/application/user/useCase/GetUserUseCase.java
@@ -0,0 +1,31 @@
+package com.monitoria.habibs.application.user.useCase;
+
+import org.springframework.stereotype.Component;
+
+import com.monitoria.habibs.application.user.in.inputPort.GetUserInputPort;
+import com.monitoria.habibs.application.user.in.output.UserOutput;
+import com.monitoria.habibs.application.user.mapping.UserMapper;
+import com.monitoria.habibs.application.user.out.UserRepository;
+import com.monitoria.habibs.domain.exception.UserNotFoundException;
+import com.monitoria.habibs.domain.model.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class GetUserUseCase implements GetUserInputPort {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserOutput getUser(String email) {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UserNotFoundException("User not found with email: " + email);
+        }
+
+        return userMapper.toOutput(user);
+    }
+}
